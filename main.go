088c2de51222ac4a080d68ade63b438619ca501b
lapis2411 <lapis2411@gmail.com>
2025-07-12 02:25:43 +0900

    feat: Setup project and draw player
    - Initialize Go module and add Ebitengine dependency.
    - Create main.go with basic game structure.
    - Implement Player and Game structs.
    - Draw player as a white rectangle on a light blue background.

--- main.go
+++ main.go
@@ -1,30 +1,80 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 400
+	screenHeight = 600
+)
 
+// Player represents the player character.
+type Player struct {
+	X, Y   float64
+	Width  float64
+	Height float64
+	Image  *ebiten.Image
+}
+
+// Game represents the main game state.
+type Game struct {
+	player *Player
+}
+
+// NewGame initializes the game.
+func NewGame() *Game {
+	playerImage := ebiten.NewImage(30, 50)
+	playerImage.Fill(color.White)
+
+	player := &Player{
+		X:      screenWidth / 2,
+		Y:      screenHeight / 2,
+		Width:  30,
+		Height: 50,
+		Image:  playerImage,
+	}
+
+	return &Game{
+		player: player,
+	}
+}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
+	// Will be implemented in PR #2
 	return nil
 }
 
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// Fill the background with light blue
+	screen.Fill(color.RGBA{R: 173, G: 216, B: 230, A: 255})
+
+	// Draw the player
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(g.player.X, g.player.Y)
+	screen.DrawImage(g.player.Image, op)
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Doodle Jump")
+
+	game := NewGame()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
